87fa3e7bb7c802f704d8f25f722ecf312ac845a3
Thanachot supawasut <48315658+thanachot30@users.noreply.github.com>
2024-10-27 15:00:42 +0700

    Add files via upload

---
+++ app_rec_movie_64130701704 (1).py
@@ -0,0 +1,34 @@
+import pickle
+import streamlit as st
+from surprise import Dataset, Reader, SVD
+
+# Load data from the file
+with open('recommendation_movie_svd.pkl', 'rb') as file:
+    svd_model, movie_ratings, movies = pickle.load(file)
+
+# Streamlit app title
+st.title("Movie Recommendation System")
+
+# User input for selecting a User ID
+user_id = st.number_input("Enter User ID:", min_value=1, step=1, value=1)
+
+# Filter rated and unrated movies
+rated_user_movies = movie_ratings[movie_ratings['userId'] == user_id]['movieId'].values
+unrated_movies = movies[~movies['movieId'].isin(rated_user_movies)]['movieId']
+
+# Predict ratings for unrated movies
+pred_rating = [svd_model.predict(user_id, movie_id) for movie_id in unrated_movies]
+
+# Sort predictions by estimated rating in descending order
+sorted_predictions = sorted(pred_rating, key=lambda x: x.est, reverse=True)
+
+# Get top 10 movie recommendations
+top_recommendations = sorted_predictions[:10]
+
+# Display top recommendations
+st.subheader(f"Top 10 movie recommendations for User {user_id}:")
+for recommendation in top_recommendations:
+    movie_title = movies[movies['movieId'] == recommendation.iid]['title'].values[0]
+    st.write(f"{movie_title} (Estimated Rating: {recommendation.est:.2f})")
+
+
